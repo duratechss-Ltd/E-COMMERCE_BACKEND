670bf1a7e696ffde1d876d5af0ae37900603a21a
duratechss-Ltd <duratechss@gmail.com>
2025-12-07 14:55:53 +0200

    made some changes to add order, authentication and product endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 const express = require("express");
+import productRoutes from "./routes/productRoutes";
+import orderRoutes from "./routes/orderRoutes";
+import authRoutes from "./routes/authRoutes";
+import type { Request, Response } from "express";
 
 export const app = express();
 const port = 3000;
-app.get("/", (req: Request, res: Response) => {
-  console.log("hello world");
-});
+
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/auth", authRoutes);
+
 app.listen(port, () => {
   console.log(`App listening on port: ${port}`);
 });
